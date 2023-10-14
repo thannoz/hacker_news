f81ca743cf76bceadc981935998794c3937ca032
thannoz <carlos-7@hotmail.de>
2023-10-14 20:37:27 +0200

    Set middleware for debuging mode

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,10 +4,18 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
+	mux.Use(middleware.RequestID)
+	mux.Use(middleware.RealIP)
+	mux.Use(middleware.Recoverer)
+
+	if app.debug {
+		mux.Use(middleware.Logger)
+	}
 	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(app.appName))
 	})
