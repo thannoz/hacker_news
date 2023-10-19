fa5ef1c88938d578c027d5a667ef24bf7e368757
thannoz <carlos-7@hotmail.de>
2023-10-19 23:20:12 +0200

    Added assets css

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
-	"github.com/CloudyKit/jet/v6"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -19,22 +17,9 @@ func (app *application) routes() http.Handler {
 	if app.debug {
 		mux.Use(middleware.Logger)
 	}
-	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		app.session.Put(r.Context(), "test", "Carlos Konzo")
-		err := app.render(w, r, "index", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
 
-	mux.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
-		vars := make(jet.VarMap)
-		vars.Set("test", app.session.GetString(r.Context(), "test"))
-		err := app.render(w, r, "index", vars)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	})
+	fileServer := http.FileServer(http.Dir("./public"))
+	mux.Handle("/public", http.StripPrefix("/public", fileServer))
 
 	return mux
 }
