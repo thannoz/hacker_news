1b8c860ee807a6cf02e449f204201014f6941aa3
thannoz <carlos-7@hotmail.de>
2023-11-18 21:26:33 +0100

    Added authentication

--- cmd/web/middlewares.go
+++ cmd/web/middlewares.go
@@ -5,3 +5,16 @@ import "net/http"
 func (app *application) LoadSession(next http.Handler) http.Handler {
 	return app.session.LoadAndSave(next)
 }
+
+func (a *application) authRequired(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		userId := a.session.GetInt(r.Context(), sessionKeyUserId)
+		if userId == 0 {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+		}
+
+		w.Header().Add("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	}
+}
