cc897f8b3521b8b7a56347db31903615b45a7d34
thannoz <carlos-7@hotmail.de>
2023-11-21 17:35:15 +0100

    Started implementing functionality for forms

---
+++ form/form.go
@@ -0,0 +1,11 @@
+package form
+
+import "regexp"
+
+var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+type errs map[string][]string
+
+func (e errs) Add(field, message string) {
+	e[field] = append(e[field], message)
+}
